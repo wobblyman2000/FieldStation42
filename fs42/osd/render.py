9f51bc38c3b99c2552453f905deeb4f64c554c8f
Your Name <your.email@example.com>
2026-09-23 14:24:29 +1000

    Safely handle Wayland set_window_pos in render.py

--- fs42/osd/render.py
+++ fs42/osd/render.py
@@ -158,7 +158,10 @@ def create_window():
     glfw.window_hint(glfw.AUTO_ICONIFY, glfw.FALSE)
     glfw.window_hint(glfw.RESIZABLE, glfw.FALSE)
     window = glfw.create_window(mode.size.width, mode.size.height, "FieldStation42 OSD", None, None)
-    glfw.set_window_pos(window, 0, 0)
+    try:
+        glfw.set_window_pos(window, 0, 0)
+    except Exception:
+        pass
     glfw.make_context_current(window)
     glfw.set_input_mode(window, glfw.CURSOR, glfw.CURSOR_DISABLED)
 
